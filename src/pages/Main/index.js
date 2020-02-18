6cf085780e43568879d5cca70d0fb7c703b88533
Manogel <manoel_ggomes@hotmail.com>
2020-02-18 12:04:35 -0300

    adding repositories

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,19 +1,46 @@
-import React from 'react';
-import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import React, { useState } from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+
 import { Container, Form, SubmitButton } from './styles';
+import api from '../../services/api';
 
 export default function Main() {
+  const [repository, setRepository] = useState('');
+  const [repositories, setRepositories] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setLoading(true);
+    const response = await api.get(`/repos/${repository}`);
+
+    const { full_name: name } = response.data;
+
+    setRepositories([...repositories, { name }]);
+    setRepository('');
+    setLoading(false);
+  }
+
   return (
     <Container>
       <h1>
         <FaGithubAlt />
         Repositórios
       </h1>
-      <Form onSubmit={() => {}}>
-        <input type="text" placeholder="Adicionar repositório" />
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Adicionar repositório"
+          value={repository}
+          onChange={e => setRepository(e.target.value)}
+        />
 
-        <SubmitButton>
-          <FaPlus color="#fff" size={14} />
+        <SubmitButton loading={loading}>
+          {loading ? (
+            <FaSpinner color="#fff" size={14} />
+          ) : (
+            <FaPlus color="#fff" size={14} />
+          )}
         </SubmitButton>
       </Form>
     </Container>
