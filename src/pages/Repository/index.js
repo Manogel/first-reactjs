df64d5c4ce40cb7359cad0a8baf8749b7ea440e5
Manogel <manoel_ggomes@hotmail.com>
2020-02-20 10:06:32 -0300

    prop types

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,35 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import { Container } from './styles';
+import api from '../../services/api';
 
 export default function Repository({ match: { params } }) {
+  const [repository, setRepository] = useState({});
+  const [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const repoName = decodeURIComponent(params.repo);
+
+    Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: { state: 'open', per_page: 5 },
+      }),
+    ]).then(([{ data: repo }, { data: issues }]) => {
+      setRepository(repo);
+      setIssues(issues);
+      setLoading(false);
+    });
+  }, []);
+
   return <Container>Repository: {decodeURIComponent(params.repo)}</Container>;
 }
+
+Repository.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      repo: PropTypes.string,
+    }),
+  }).isRequired,
+};
